a77f79f68ae31291c21a665c942f668932642895
Jenequaintech <jtho110@wgu.edu>
2024-04-14 18:25:11 -0500

    elpSQL

--- app.py
+++ app.py
@@ -1,69 +1,72 @@
-from flask import Flask
-from routes import configure_routes
+from flask import Flask, request, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
-def create_app():
-    app = Flask(__name__)
-    configure_routes(app)  # This will set up your routes from routes.py
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
-
-  from flask import Flask, jsonify, request, abort
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# In-memory 'database' for movies
-movies = [
-    {'id': 1, 'title': 'Love and Basketball', 'director': 'Gina Prince-Bythewood', 'release_year': 2000},
-    {'id': 2, 'title': "What's Love Got to Do with It", 'director': 'Brian Gibson', 'release_year': 1993},
-    {'id': 3, 'title': 'The Color Purple', 'director': 'Steven Spielberg', 'release_year': 1985}
-]
+db = SQLAlchemy(app)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    director = db.Column(db.String())
+    release_year = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return "Connected to the database!"
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    return jsonify(movies)
+    movies = Movie.query.all()
+    return jsonify({'movies': [{'id': movie.id, 'title': movie.title, 'director': movie.director, 'release_year': movie.release_year} for movie in movies]})
 
 @app.route('/movies', methods=['POST'])
-def create_movie():
+def add_movie():
     if not request.json or not 'title' in request.json:
         abort(400)
-    new_movie = {
-        'id': movies[-1]['id'] + 1,
-        'title': request.json['title'],
-        'director': request.json.get('director', ""),
-        'release_year': request.json.get('release_year', "")
-    }
-    movies.append(new_movie)
-    return jsonify(new_movie), 201
+    movie = Movie(
+        title=request.json['title'],
+        director=request.json.get('director', ''),
+        release_year=request.json.get('release_year')
+    )
+    db.session.add(movie)
+    db.session.commit()
+    return jsonify({'id': movie.id, 'title': movie.title}), 201
 
 @app.route('/movies/<int:movie_id>', methods=['PUT'])
 def update_movie(movie_id):
-    movie = [movie for movie in movies if movie['id'] == movie_id]
-    if len(movie) == 0:
+    movie = Movie.query.get(movie_id)
+    if not movie:
         abort(404)
     if not request.json:
         abort(400)
-    if 'title' in request.json and type(request.json['title']) != str:
-        abort(400)
-    if 'director' in request.json and type(request.json['director']) is not str:
-        abort(400)
-    if 'release_year' in request.json and type(request.json['release_year']) is not int:
-        abort(400)
-
-    movie[0]['title'] = request.json.get('title', movie[0]['title'])
-    movie[0]['director'] = request.json.get('director', movie[0]['director'])
-    movie[0]['release_year'] = request.json.get('release_year', movie[0]['release_year'])
-    return jsonify(movie[0])
+    movie.title = request.json.get('title', movie.title)
+    movie.director = request.json.get('director', movie.director)
+    movie.release_year = request.json.get('release_year', movie.release_year)
+    db.session.commit()
+    return jsonify({'id': movie.id, 'title': movie.title})
 
 @app.route('/movies/<int:movie_id>', methods=['DELETE'])
 def delete_movie(movie_id):
-    movie = [movie for movie in movies if movie['id'] == movie_id]
-    if len(movie) == 0:
+    movie = Movie.query.get(movie_id)
+    if not movie:
         abort(404)
-    movies.remove(movie[0])
+    db.session.delete(movie)
+    db.session.commit()
     return jsonify({'result': True})
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
